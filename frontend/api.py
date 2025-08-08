aaa08a2e183f347207dcd22afafdec05ed57bb05
Jonas Thiele <99022483+very-many@users.noreply.github.com>
2025-08-08 10:28:29 +0200

    Working but ugly

--- frontend/api.py
+++ frontend/api.py
@@ -12,4 +12,13 @@ def create_url(url: str) -> str:
     api_url = f"{BACKEND_URL}/url"
     response = requests.post(api_url, headers=headers, json=body)
     response.raise_for_status()
-    return response.json().get("url", "")
+    return response.json()
+
+def get_admin_info(admin_key: str) -> str:
+    headers = {
+        "Content-Type": "application/json",
+    }
+    api_url = f"{BACKEND_URL}/admin/{admin_key}"
+    response = requests.get(api_url, headers=headers)
+    response.raise_for_status()
+    return response.json()
--- frontend/main.py
+++ frontend/main.py
@@ -1,40 +1,121 @@
-from nicegui import ui
-from api import create_url
+from unittest import result
+from nicegui import PageArguments, ui
+from api import create_url, get_admin_info
 
 
-ui.colors(primary="#7e57c2", secondary="#26c6da", accent="#ff7043")
+def on_submit(url: str):
+    try:
+        result = create_url(url)
+        result_area.visible = True
+        result_area.clear()
+        with result_area:
+            with ui.label("Your shortened URL: "):
+                ui.link(result.get('url', ''), result.get('url', ''), new_tab=True)
+            with ui.label("Your original URL: "):
+                ui.link(result.get('target_url', ''), result.get('target_url', ''), new_tab=True)
+            with ui.label("Admin Key: "):
+                ui.link(result.get('admin_url', '').split("/")[-1], f"/admin?key={result.get('admin_url', '').split('/')[-1]}")
+        ui.notify("URL shortened successfully!", color="green")
+    except Exception as e:
+        ui.notify(f"Error: {str(e)}", color="red")
+        
+def on_admin_submit(admin_key: str):
+    if not admin_key:
+        ui.notify("Please enter an admin key.", color="red")
+        return
+    try:
+        result = get_admin_info(admin_key)
+        result_area.visible = True
+        result_area.clear()
+        with result_area:
+            with ui.label("Shortened URL: "):
+                ui.link(result.get('url', ''), result.get('url', ''), new_tab=True)
+            with ui.label("Original URL: "):
+                ui.link(result.get('target_url', ''), result.get('target_url', ''), new_tab=True)
+            with ui.label("Clicks: "):
+                ui.label(str(result.get('clicks', 0)))
+            if result.get('is_active', False) == True:
+                ui.label("Status: Active")
+            else:
+                ui.label("Status: Inactive")
+        ui.notify("Admin info retrieved successfully!", color="green")
+    except Exception as e:
+        ui.notify(f"Error: {str(e)}", color="red")
 
-dark = ui.dark_mode()
-old_img_url = ""
+
+@ui.page("/")
+@ui.page("/admin")
+def index():
+    ui.colors(primary="#7e57c2", secondary="#26c6da", accent="#ff7043")
+    dark = ui.dark_mode()
+    # bg img
+    ui.element("img").props(
+        'src="https://bin-nich-kreativ.de/_astro/background.BPKAcmfN.svg"'
+    ).classes("fixed top-0 left-0 w-screen h-screen object-cover -z-10 blur-[100px]")
+
+    # main layout
+    with ui.column().classes("w-full m-auto items-center gap-8"):
+        title = ui.link("URL Shortener", "/").classes(
+            "text-4xl font-bold bg-gradient-to-r from-purple-500 to-cyan-400 bg-clip-text text-transparent mt-12 text-center"
+        )
+        ui.switch().bind_value(dark).props("flat round color=primary").tooltip(
+            "Toggle dark mode"
+        ).classes("absolute top-4 right-4")
+        ui.sub_pages({"/": main, "/admin": admin}, data={"title": title}).classes("w-full max-w-[1024px]")
 
 
-def toggle_dark_mode():
-    global dark_mode
-    dark_mode = not dark_mode
-    if dark_mode:
-        ui.dark_mode().enable()
-    else:
-        ui.dark_mode().disable()
-
-
-# bg img
-ui.element("img").props(
-    'src="https://bin-nich-kreativ.de/_astro/background.BPKAcmfN.svg"'
-).classes("fixed top-0 left-0 w-screen h-screen object-cover -z-10 blur-[100px]")
-
-
-# main layout
-with ui.column().classes("w-full m-auto items-center gap-8"):
-    ui.label("API Shortener").classes(
-        "text-4xl font-bold bg-gradient-to-r from-purple-500 to-cyan-400 bg-clip-text text-transparent mt-8"
+def main(title: ui.label):
+    title.text = "URL Shortener"
+    ui.button("Go to Admin page", on_click=lambda: ui.navigate.to("/admin")).classes(
+        "absolute top-4 left-4"
+    ).props("color=primary")
+    with ui.row().classes("w-full justify-center gap-4 items-center"):
+        url_to_shorten_box = (
+            ui.input(
+                label="URL to shorten",
+                placeholder="Enter URL here",
+                value="https://bin-nich-kreativ.de",
+            )
+            .classes("w-full max-w-2xl")
+            .props("clearable")
+        )
+        shorten_button = ui.button(
+            "Shorten", on_click=lambda: on_submit(url_to_shorten_box.value)
+        ).props("color=primary")
+        
+    global result_area
+    result_area = ui.column().classes(
+        "w-full max-w-[1024px] flex flow-col justify-center p-8 shadow-lg rounded-xl bg-gray-200/60 dark:bg-gray-800/60"
     )
-
-    ui.switch().bind_value(dark).props("flat round color=primary").tooltip(
-        "Toggle dark mode"
-    ).classes("absolute top-4 right-4")
-    ui.label(create_url("https://example.com")).classes("text-lg")
+    result_area.visible = False
 
 
+def admin(args: PageArguments, title: ui.label):
+    key = args.query_parameters.get("key", "")
+    title.text = "URL Shortener Admin Page"
+    ui.label("Admin page content")
+    ui.button("Go to main page", on_click=lambda: ui.navigate.to("/")).classes(
+        "absolute top-4 left-4"
+    ).props("color=primary")
+    with ui.row().classes("w-full justify-center gap-4 items-center"):
+        admin_key_box = (
+            ui.input(
+                label="Admin Key",
+                placeholder="Enter Admin Key here",
+                value=key if key else "BQEON_XHQGE93U",
+            )
+            .classes("w-full max-w-2xl")
+            .props("clearable")
+        )
+        admin_button = ui.button(
+            "get info", on_click=lambda: on_admin_submit(admin_key_box.value)
+        ).props("color=primary")
+        
+    global result_area
+    result_area = ui.column().classes(
+        "w-full max-w-[1024px] flex flow-col justify-center p-8 shadow-lg rounded-xl bg-gray-200/60 dark:bg-gray-800/60"
+    )
+    result_area.visible = False
 
 
 ui.run(favicon="🔗", title="API Shortener")
